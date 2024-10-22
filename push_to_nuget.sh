ad205646ecf095085e7d611a9d5043bdec11f8e8
shokhzodmurodov <shaxa882@gmail.com>
2024-10-22 16:33:54 +0500

    changed version

--- push_to_nuget.sh
+++ push_to_nuget.sh
@@ -1,3 +1,3 @@
 dotnet build --configuration Release
 dotnet pack --configuration Release
-nuget push ./EntityLib/bin/Release/Maxwell.EntityLib.1.0.1.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push ./EntityLib/bin/Release/Maxwell.EntityLib.1.0.2.nupkg -Source https://api.nuget.org/v3/index.json
